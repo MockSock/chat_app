4980a7c21d5f939a26ff85eb66a2b642533557a8
madden20202 <business.mocksock@gmail.com>
2023-01-21 16:46:57 -0600

    finish off decoder for chat app

--- lib/services.dart
+++ lib/services.dart
@@ -1,15 +1,24 @@
 import 'package:dio/dio.dart';
+import 'dart:convert';
+
 import 'models/user.dart';
 
 class NetWorkServices {
   String url =
       'https://console.firebase.google.com/u/2/project/chat-app-92984/database/chat-app-92984-default-rtdb/data/~2F';
 
-  getUsers() async {
-    try {
-      var response = await Dio().get(url);
+  List<User> usersDecoder(String str) =>
+      List<User>.from(json.decode(str).map((x) => User.fromJson(x)));
 
-      return response;
+  Future<List<User>> userResponseHandler(Response response) async {
+    List<User> users = usersDecoder(response.data);
+    return users;
+  }
+
+  getUsersJson() async {
+    try {
+      Response response = await Dio().get(url);
+      return userResponseHandler(response);
     } catch (error) {
       print(error);
     }
