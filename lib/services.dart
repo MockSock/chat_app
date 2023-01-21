544f02eebbf1de260d802a9731410729359f52cb
madden20202 <business.mocksock@gmail.com>
2023-01-21 16:19:56 -0600

    move url to class level

--- lib/services.dart
+++ lib/services.dart
@@ -2,10 +2,10 @@ import 'package:dio/dio.dart';
 import 'models/user.dart';
 
 class NetWorkServices {
-  getUsers() async {
-    String url =
-        'https://console.firebase.google.com/u/2/project/chat-app-92984/database/chat-app-92984-default-rtdb/data/~2F';
+  String url =
+      'https://console.firebase.google.com/u/2/project/chat-app-92984/database/chat-app-92984-default-rtdb/data/~2F';
 
+  getUsers() async {
     try {
       var response = await Dio().get(url);
 
