3411693ee7b2cf5916c1f081a3ad4299ed5827a9
madden20202 <business.mocksock@gmail.com>
2023-01-21 16:07:14 -0600

    add getUsers and

--- lib/services.dart
+++ lib/services.dart
@@ -1,2 +1,13 @@
 import 'package:dio/dio.dart';
 import 'models/user.dart';
+
+void getUsers() async {
+  String url =
+      'https://console.firebase.google.com/u/2/project/chat-app-92984/database/chat-app-92984-default-rtdb/data/~2F';
+
+  try {
+    var response = await Dio().get(url);
+  } catch (error) {
+    print(error);
+  }
+}
