7e1ef648f85c8d85eeb65247514ecbe84c9b0b7b
madden20202 <business.mocksock@gmail.com>
2023-01-21 15:51:24 -0600

    move chat list to its own widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'models/user.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -31,54 +29,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // Dummy data
-  final List<User> userList = [
-    User(
-      userId: 24601,
-      userName: 'ValjeansOnSale',
-    ),
-    User(
-      userId: 43324,
-      userName: '404NeverFound',
-    ),
-    User(
-      userId: 20202,
-      userName: 'BackToBackFutureInc',
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          ListView.builder(
-              itemCount: userList.length,
-              itemBuilder: (context, index) {
-                if (index.isOdd) return const Divider();
-
-                return Card(
-                  elevation: 4.0,
-                  color: Theme.of(context).primaryColor,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Column(
-                        children: const [
-                          Text('Test'),
-                          Text('123'),
-                        ],
-                      ),
-                      const Text('I would be a message!'),
-                    ],
-                  ),
-                );
-              })
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
       ),
     );
   }
