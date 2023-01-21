f3324603fbe1a0c2282cf826ae662c85c6c9117b
madden20202 <business.mocksock@gmail.com>
2023-01-21 16:18:43 -0600

    finish off get users

--- lib/services.dart
+++ lib/services.dart
@@ -1,13 +1,17 @@
 import 'package:dio/dio.dart';
 import 'models/user.dart';
 
-void getUsers() async {
-  String url =
-      'https://console.firebase.google.com/u/2/project/chat-app-92984/database/chat-app-92984-default-rtdb/data/~2F';
+class NetWorkServices {
+  getUsers() async {
+    String url =
+        'https://console.firebase.google.com/u/2/project/chat-app-92984/database/chat-app-92984-default-rtdb/data/~2F';
 
-  try {
-    var response = await Dio().get(url);
-  } catch (error) {
-    print(error);
+    try {
+      var response = await Dio().get(url);
+
+      return response;
+    } catch (error) {
+      print(error);
+    }
   }
 }
