b04af991c1089bd805f7099054cfa0e47f8d7024
madden20202 <business.mocksock@gmail.com>
2023-01-22 16:18:14 -0600

    initialize firebase app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'widgets/chat_list.dart';
 import 'models/chat.dart';
 import 'services.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
