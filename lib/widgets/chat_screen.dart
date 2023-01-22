d4ce8ef175d29d49ff877db1222d28f827b48841
madden20202 <business.mocksock@gmail.com>
2023-01-22 12:05:29 -0600

    add custom chat bar

--- lib/widgets/chat_screen.dart
+++ lib/widgets/chat_screen.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
-  const ChatScreen({
+  ChatScreen({
     super.key,
     required this.chatId,
+    required this.chatName,
   });
 
   final String chatId;
+  final String chatName;
+
   @override
   Widget build(BuildContext context) {
-    return Text(chatId);
+    final chatAppBar = AppBar(
+      title: const Text('Chat name will be here'),
+      actions: [
+        IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(Icons.arrow_back),
+        )
+      ],
+    );
+    return Scaffold(
+      appBar: chatAppBar,
+      body: Text(chatId),
+    );
   }
 }
