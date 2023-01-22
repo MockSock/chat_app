73a6904a39fcb49fa25b5372cde2734c8a324ee7
madden20202 <business.mocksock@gmail.com>
2023-01-21 20:16:18 -0600

    Add chartContent and lastUpdated

--- lib/models/chat.dart
+++ lib/models/chat.dart
@@ -1,14 +1,22 @@
+// ignore_for_file: file_names
+
 class Chat {
   Chat({
     required this.chatId,
     required this.chatName,
+    required this.chatContent,
+    required this.lastUpdated,
   });
 
   final int chatId;
   final String chatName;
+  final String chatContent;
+  final DateTime lastUpdated;
 
   factory Chat.fromJson(Map<String, dynamic> json) => Chat(
         chatId: json['chatId'],
         chatName: json['chatname'],
+        chatContent: json['chatContent'],
+        lastUpdated: json['lastUpdated'],
       );
 }
