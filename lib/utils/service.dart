828678892a6c72ee2e75a4ed4937abb322b79b0e
madden20202 <business.mocksock@gmail.com>
2023-01-25 20:25:31 -0600

    add beginning of network service

--- lib/utils/service.dart
+++ lib/utils/service.dart
@@ -1,2 +1,23 @@
 // This file will condense all firebase work in one file
 
+import 'package:firebase_auth/firebase_auth.dart';
+import '../models/user.dart';
+
+class NetworkService {
+  // Always require currentUser for security
+  NetworkService({required this.currentUser});
+
+  AppUser currentUser;
+
+  checkUserStatus() async {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user != null) {
+        // find way to repackage it and pass it along
+        currentUser = user;
+        return currentUser;
+      } else {
+        return;
+      }
+    });
+  }
+}
