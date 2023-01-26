356a762ead5bc404f7fae79531a706a25bd372bc
madden20202 <business.mocksock@gmail.com>
2023-01-25 20:04:15 -0600

    add very basic User class

---
+++ lib/models/user.dart
@@ -0,0 +1,11 @@
+class User {
+  const User({
+    required this.userId,
+    required this.userName,
+  });
+
+  //Super bare bones for this moment
+  final int userId;
+  final String userName;
+  // Will want photos in future
+}
