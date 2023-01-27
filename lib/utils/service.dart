09da9ea23d4d0e972ec30477ce62970b906d75b6
madden20202 <business.mocksock@gmail.com>
2023-01-26 18:26:22 -0600

    remove appUser in exchange for firebase auth user

--- lib/utils/service.dart
+++ lib/utils/service.dart
@@ -1,20 +1,15 @@
 // This file will condense all firebase work in one file
 
 import 'package:firebase_auth/firebase_auth.dart';
-import '../models/user.dart';
 
 class NetworkService {
   // Always require currentUser for security
-  NetworkService({required this.currentUser});
-
-  AppUser currentUser;
+  // NetworkService({});
 
   checkUserStatus() async {
     FirebaseAuth.instance.authStateChanges().listen((User? user) {
       if (user != null) {
-        // find way to repackage it and pass it along
-        currentUser = user;
-        return currentUser;
+        // Firebase auth has its own user parameters use those
       } else {
         return;
       }
