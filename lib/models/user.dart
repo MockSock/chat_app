c554b10523d0271f78ef70ebd6c05a072ef1fc49
madden20202 <business.mocksock@gmail.com>
2023-01-26 18:57:57 -0600

    Revert "remove appUser in exchange for firebase auth user"
    This reverts commit 09da9ea23d4d0e972ec30477ce62970b906d75b6.

---
+++ lib/models/user.dart
@@ -0,0 +1,11 @@
+class AppUser {
+  const AppUser({
+    required this.userId,
+    required this.userName,
+  });
+
+  //Super bare bones for this moment
+  final int userId;
+  final String userName;
+  // Will want photos in future
+}
--- lib/utils/service.dart
+++ lib/utils/service.dart
@@ -1,15 +1,20 @@
 // This file will condense all firebase work in one file
 
 import 'package:firebase_auth/firebase_auth.dart';
+import '../models/user.dart';
 
 class NetworkService {
   // Always require currentUser for security
-  // NetworkService({});
+  NetworkService({required this.currentUser});
+
+  AppUser currentUser;
 
   checkUserStatus() async {
     FirebaseAuth.instance.authStateChanges().listen((User? user) {
       if (user != null) {
-        // Firebase auth has its own user parameters use those
+        // find way to repackage it and pass it along
+        currentUser = user;
+        return currentUser;
       } else {
         return;
       }
