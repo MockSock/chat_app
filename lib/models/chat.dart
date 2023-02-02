a7a06b1b2b82f06490f0fc8b114ecbc3d2960d5d
madden20202 <business.mocksock@gmail.com>
2023-02-01 19:22:16 -0600

    remove unused factory from chat.dart

--- lib/models/chat.dart
+++ lib/models/chat.dart
@@ -10,10 +10,4 @@ class Chat {
   final int chatId;
   final String chatName;
   final DateTime lastUpdated;
-
-  factory Chat.fromJson(Map<String, dynamic> json) => Chat(
-        chatId: json['chatId'],
-        chatName: json['chatname'],
-        lastUpdated: json['lastUpdated'],
-      );
 }
