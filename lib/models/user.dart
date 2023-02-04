841dc870671d1c2b6c58d0d97e5aad2ae56bc170
madden20202 <business.mocksock@gmail.com>
2023-02-03 18:41:00 -0600

    add bool for if user is current user

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,11 +2,13 @@ class AppUser {
   AppUser({
     required this.userId,
     required this.userName,
+    required this.isCurrentUser,
   });
 
   //Super bare bones for this moment
   // Firebase auth has this as a String?
   String? userId;
   String userName;
+  bool isCurrentUser;
   // Will want photos in future
 }
