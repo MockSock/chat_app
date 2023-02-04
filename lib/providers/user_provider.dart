25bbb835fd27ec611ea6d0167c45e547527b5213
madden20202 <business.mocksock@gmail.com>
2023-02-03 18:42:00 -0600

    make _appUsers accessable via getter

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,3 +1,15 @@
 import 'package:flutter/material.dart';
 
-class AppUsers with ChangeNotifier {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../models/user.dart';
+
+class AppUsers with ChangeNotifier {
+  // Private list
+  List<AppUser> _appUsers = [];
+
+  // Getter for firebase list of users
+  List<AppUser> get appUsers {
+    return _appUsers;
+  }
+}
