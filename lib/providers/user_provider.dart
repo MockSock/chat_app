95b873379a6613334de8f03fe44c1135a03c3162
madden20202 <business.mocksock@gmail.com>
2023-02-11 15:41:56 -0600

    change provider to give dummy data

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -5,8 +5,21 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/user.dart';
 
 class AppUsers with ChangeNotifier {
-  // Private list
-  List<AppUser> _appUsers = [];
+  // Private list that needs to get the firebase users
+  final List<AppUser> _appUsers = [
+    // dummy data
+    AppUser(
+      userId: 'SB09',
+      userName: 'KaylaLova',
+      isCurrentUser: false,
+    ),
+
+    AppUser(
+      userId: 'JJ34',
+      userName: 'TimTam11',
+      isCurrentUser: false,
+    ),
+  ];
 
   // Getter for firebase list of users
   List<AppUser> get appUsers {
