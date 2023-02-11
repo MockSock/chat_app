a8c4f5c89f057c21ce96789de32af697b1ee25c4
madden20202 <business.mocksock@gmail.com>
2023-02-11 16:05:09 -0600

    remove multiprovider since it wont be used

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
+import 'providers/user_provider.dart';
 
 import 'widgets/chat_list.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: ,
-        ),
-      ],
-      child: const MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -30,7 +22,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.cyan,
       ),
-      home: ChatList(chatList: chatList),
+      home: ChatList(),
+      // Routes will be here one day
+      // routes: ,
     );
   }
 }
