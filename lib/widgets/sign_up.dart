5d860de270c3145607dbf5361410cad622377e17
madden20202 <business.mocksock@gmail.com>
2023-02-11 15:55:34 -0600

    add dummy widget for sign-up.dart

--- lib/widgets/sign_up.dart
+++ lib/widgets/sign_up.dart
@@ -1,1 +1,11 @@
 // This will be for when a user does not want to be an anonymous user
+import 'package:flutter/material.dart';
+
+class SignUp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text('I Will be a signup page one day!'),
+    );
+  }
+}
