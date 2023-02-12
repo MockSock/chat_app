fd738a1c8e0ab4c8261652c42311d56d2b43cfde
madden20202 <business.mocksock@gmail.com>
2023-02-11 18:38:57 -0600

    remove uneeded model import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:chat_app/models/user.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'package:provider/provider.dart';
 import 'providers/user_provider.dart';
