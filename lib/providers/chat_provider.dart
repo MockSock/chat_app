8387213e320252881a9b91e1e770167d9eb2ccf9
madden20202 <business.mocksock@gmail.com>
2023-02-11 19:01:19 -0600

    fill out dummy data

--- lib/providers/chat_provider.dart
+++ lib/providers/chat_provider.dart
@@ -1,3 +1,21 @@
 import 'package:flutter/material.dart';
 
 import '../models/chat.dart';
+
+class ActiveChats extends ChangeNotifier {
+  final List<Chat> _currentChats = [
+    // dummy data
+    Chat(
+      chatId: 10,
+      chatName: 'Party Hauss',
+    ),
+    Chat(
+      chatId: 34,
+      chatName: 'Dev Meetings',
+    ),
+    Chat(
+      chatId: 87,
+      chatName: 'Secret Dev Meetings',
+    ),
+  ];
+}
