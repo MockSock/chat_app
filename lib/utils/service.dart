c251ff040247655a1185b93a1c8cacc69bd00e7a
madden20202 <business.mocksock@gmail.com>
2023-02-13 10:18:50 -0600

    Add basic crud methods

--- lib/utils/service.dart
+++ lib/utils/service.dart
@@ -1,15 +1,34 @@
 // This file will condense all firebase work in one file
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import '../models/user.dart';
+import '../models/chat.dart';
 
 class NetworkService {
   // Always require currentUser for security
   NetworkService({required this.currentUser});
-
   AppUser currentUser;
 
-  Future checkUserStatus() async {
+  // CRUD operations
+  Future<void> createChat() async {
+    return;
+  }
+
+  Future<void> deleteChat() async {
+    return;
+  }
+
+  Stream<List<Chat>> chatStream() {
+    return;
+  }
+
+  Stream<AppUser> userStream() {
+    return;
+  }
+
+  Future<void> checkUserStatus() async {
     FirebaseAuth.instance.authStateChanges().listen((User? user) {
       if (user != null) {
         // A for loop would let you iterate through
