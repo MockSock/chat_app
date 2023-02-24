453a09387c28e493dbb0bdd529fb74957504712e
madden20202 <business.mocksock@gmail.com>
2023-02-24 10:45:28 -0600

    begin change to streambuilder

--- lib/widgets/chat_list.dart
+++ lib/widgets/chat_list.dart
@@ -9,47 +9,41 @@ class ChatList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final chatListData = Provider.of<Chat>(context);
-    final chatList = chatListData;
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        // Will need to change to a stream builder once firebase is set up
-        ListView.builder(
-            itemCount: chatList.length,
-            itemBuilder: (context, index) {
-              if (index.isOdd) {
-                return Divider(color: Theme.of(context).dividerColor);
-              }
+    return StreamBuilder(
+        initialData: chatListData,
+        stream: chatListData,
+        builder: (context, index) {
+          if (index.isOdd) {
+            return Divider(color: Theme.of(context).dividerColor);
+          }
 
-              return GestureDetector(
-                onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ChatScreen(
-                        chatId: chatList[index].chatId.toString(),
-                        chatName: chatList[index].chatName),
-                  ),
-                ),
-                child: Card(
-                  elevation: 4.0,
-                  color: Theme.of(context).primaryColor,
-                  shadowColor: Theme.of(context).shadowColor,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          return GestureDetector(
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ChatScreen(
+                    chatId: snapshot[index].chatId.toString(),
+                    chatName: chatList[index].chatName),
+              ),
+            ),
+            child: Card(
+              elevation: 4.0,
+              color: Theme.of(context).primaryColor,
+              shadowColor: Theme.of(context).shadowColor,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Column(
                     children: <Widget>[
-                      Column(
-                        children: <Widget>[
-                          Text(chatList[index].chatName),
-                          // Shows time of last text
-                          // Text(chatList[index].lastUpdated.toString()),
-                        ],
-                      ),
+                      Text(chatList[index].chatName),
+                      // Shows time of last text
+                      // Text(chatList[index].lastUpdated.toString()),
                     ],
                   ),
-                ),
-              );
-            }),
-      ],
-    );
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
