265b1c948ee3d632924fcc028840f49fbca62cb3
madden20202 <business.mocksock@gmail.com>
2023-03-05 13:20:03 -0600

    filling out nethods

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -3,6 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../models/user.dart';
+
 enum Status {
   // This allows us to cycle through all the
   // different types of authentication situations
@@ -20,6 +22,8 @@ class AuthProvider extends ChangeNotifier {
 
   Status _status = Status.uninitialized;
 
+  late AppUser appUser;
+
   Status get status => _status;
 
   AuthProvider({
@@ -27,4 +31,22 @@ class AuthProvider extends ChangeNotifier {
     required this.firebaseFirestore,
     required this.sharedPreferences,
   });
+
+  String? getFirebaseUserId() {
+    if (appUser.userId == ' ') {
+      // has to be a better way to handle errors
+      return ErrorDescription('No current user').toString();
+    } else {
+      return sharedPreferences.getString(appUser.userId.toString());
+    }
+  }
+
+  Future<bool> isUserLoggedIn() async {
+    if (firebaseAuth.currentUser != null) {
+      // make a better check for data in the future
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
